213508e49ada42223fb3b2b5175da5b3b562f916
Pedro Ramirez <prlara2000@gmail.com>
2024-09-11 23:37:14 -0500

    [feat]: add components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { EventListComponent } from './components/event-list/event-list.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [FooterComponent, HeaderComponent, EventListComponent], // Import all used components here
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
