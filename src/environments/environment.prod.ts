054d24a3c14907b1d1c896c0fe57f63b7d4499d3
Pedro Ramirez <prlara2000@gmail.com>
2024-10-15 17:34:46 -0500

    Remove environments folder from Git tracking

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  emailJsPublicKey: 'pr1UJGTPULYPKkq54',
-  emailJsServiceId: 'service_08bd9yl',
-  emailJsTemplateId: 'template_vwnfl19',
-};
--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  emailJsPublicKey: 'pr1UJGTPULYPKkq54',
-  emailJsServiceId: 'service_08bd9yl',
-  emailJsTemplateId: 'template_vwnfl19',
-};
