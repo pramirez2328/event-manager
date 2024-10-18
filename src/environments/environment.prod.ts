10e9bb535a8d6e16746a18829c6df2c207e0feee
Pedro Ramirez <prlara2000@gmail.com>
2024-10-17 20:38:13 -0500

    fix: add envrionments

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://event-manager-kha9.onrender.com',
+  emailJsPublicKey: 'pr1UJGTPULYPKkq54',
+  emailJsServiceId: 'service_08bd9yl',
+  emailJsTemplateId: 'template_vwnfl19',
+  googleApiKey: 'AIzaSyB9KLMTdAlL65-25HVmeFTHNj_3p-EcYJ0',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:4200',
+  emailJsPublicKey: 'pr1UJGTPULYPKkq54',
+  emailJsServiceId: 'service_08bd9yl',
+  emailJsTemplateId: 'template_vwnfl19',
+  googleApiKey: 'AIzaSyB9KLMTdAlL65-25HVmeFTHNj_3p-EcYJ0',
+};
